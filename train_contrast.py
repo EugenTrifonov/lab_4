18ee111dbf4a5552044e3d49078d0417c5cacbbc
EugenTrifonov <80068414+EugenTrifonov@users.noreply.github.com>
2021-03-26 10:56:12 +0300

    Update train_contrast.py

--- train_contrast.py
+++ train_contrast.py
@@ -55,7 +55,7 @@ def create_dataset(filenames, batch_size):
   """
   return tf.data.TFRecordDataset(filenames)\
     .map(parse_proto_example, num_parallel_calls=tf.data.AUTOTUNE)\
-    .map(random_brightness)
+    .map(random_brightness)\
     .cache()\
     .batch(batch_size)\
     .prefetch(tf.data.AUTOTUNE)
