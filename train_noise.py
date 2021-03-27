11c936f4ef92673ecf11b22822dea8a1e65b3788
EugenTrifonov <80068414+EugenTrifonov@users.noreply.github.com>
2021-03-28 01:50:46 +0300

    Update train_noise.py

--- train_noise.py
+++ train_noise.py
@@ -66,8 +66,8 @@ tf.keras.layers.GaussianNoise(0.1)
 def build_model():
   inputs = tf.keras.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
   #x1=tf.keras.layers.GaussianNoise(0.05)(inputs)
-  new_input=data_augmentation(inputs)
-  model = EfficientNetB0(include_top=False,input_tensor=new_input,weights="imagenet")
+  #new_input=data_augmentation(inputs)
+  model = EfficientNetB0(include_top=False,input_tensor=inputs,weights="imagenet")
   model.trainable=False
   x = tf.keras.layers.GlobalAveragePooling2D()(model.output)
   outputs = tf.keras.layers.Dense(NUM_CLASSES,activation=tf.keras.activations.softmax)(x)
