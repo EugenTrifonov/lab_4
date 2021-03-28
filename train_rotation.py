4ac012845c07e687c44175b49a0b0d844c08f2b7
EugenTrifonov <80068414+EugenTrifonov@users.noreply.github.com>
2021-03-28 23:51:44 +0300

    Update train_rotation.py

--- train_rotation.py
+++ train_rotation.py
@@ -55,15 +55,11 @@ def create_dataset(filenames, batch_size):
     .cache()\
     .batch(batch_size)\
     .prefetch(tf.data.AUTOTUNE)
-data_augmentation = keras.Sequential(
-    [
-tf.keras.layers.experimental.preprocessing.RandomRotation(0.05)
-    ]
-)
+
 
 def build_model():
   inputs = tf.keras.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
-  new_input=data_augmentation(inputs)
+  new_input=tf.keras.layers.experimental.preprocessing.RandomRotation(0.05)(inputs)
   model = EfficientNetB0(include_top=False,input_tensor=new_input,weights="imagenet")
   model.trainable=False
   x = tf.keras.layers.GlobalAveragePooling2D()(model.output)
