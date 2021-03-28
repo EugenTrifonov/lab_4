72880655b14515c0c0618a2207c96884678df4bf
EugenTrifonov <80068414+EugenTrifonov@users.noreply.github.com>
2021-03-28 15:53:10 +0300

    Update train_rotation.py

--- train_rotation.py
+++ train_rotation.py
@@ -57,7 +57,7 @@ def create_dataset(filenames, batch_size):
     .prefetch(tf.data.AUTOTUNE)
 data_augmentation = keras.Sequential(
     [
-tf.keras.layers.experimental.preprocessing.RandomRotation(0.1)
+tf.keras.layers.experimental.preprocessing.RandomRotation(0.2)
     ]
 )
 
